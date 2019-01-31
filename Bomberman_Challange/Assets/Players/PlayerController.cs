45a93ef9e491bbfd48d9b3be6362d32ec6f22fff
Anurag.hacker <Anurag.Charp@gmail.com>
2019-01-31 19:40:47 +0530

    Added Player Scripts
    Wrting Logics for Player Movements ! and Animations !

---
+++ Bomberman_Challange/Assets/Players/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour {
+
+	public float pVertical;
+	public float pHorizontal;
+	public Animator pAnimator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		pAnimator = GetComponent<Animator>();			
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		pVertical = Input.GetAxis("Vertical");
+		pHorizontal = Input.GetAxis("Horizontal");
+		pAnimator.SetFloat("Vertical", pVertical);
+		pAnimator.SetFloat("Horizontal", pVertical);
+
+	}
+}
